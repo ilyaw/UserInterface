a66e3d3835babc5958de2b618b10bf69584fe599
Ilya <webilya29@icloud.com>
2021-02-03 15:41:53 +0300

    Create Project

---
+++ UserInterface/UserInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInterface
+//
+//  Created by Илья Руденко on 03.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
